7ec69ea858cbd900da97f4cae2b69f0d8afc83b8
Travis Rigg <rigg.travis@gmail.com>
2021-02-01 20:35:26 -0500

    Now x clear win should be easy to install

--- st.c
+++ st.c
@@ -1890,10 +1890,8 @@ strhandle(void)
 				fprintf(stderr, "erresc: invalid color j=%d, p=%s\n",
 				        j, p ? p : "(null)");
 			} else {
-				/*
-				 * TODO if defaultbg color is changed, borders
-				 * are dirty
-				 */
+				if (j == defaultbg)
+					xclearwin();
 				redraw();
 			}
 			return;
